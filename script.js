8c27b995bef13ed31244970432a35d7fa8fee662
Riley Tate <124rtate@frhsd.com>
2022-11-30 08:18:12 -0500

    .

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-var btns = document.querySelectorAll('#book-list.delete');
+const list = document.querySelector('#book-list ul');
+const forms = document.forms;
 
-Array.from(btns).forEach(function(btn){
-    btn.addEventListener('click',function(e){
-
-        const li = e.target.parentElement;
-
-        li.parentNode.removeChild(li);
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
 });
-});
-const link = document.querySelector('#page-banner a');
 
-link.addEventListener('click',function(e){
-    e.preventDefault();
-    console.log('navigation to',e.target.textContent,'was prevented');
+
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+
+  // create elements
+
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  //add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+  
+
 });
